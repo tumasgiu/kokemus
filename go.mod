e33a8c8186411bbadb081e1817202657ce3973a7
Thomas Franceschini <thomas.franceschini@groundhogsec.com>
2019-03-29 16:23:17 +0100

    feat(wip): connect to a postgres db to test connectivity

--- go.mod
+++ go.mod
@@ -1,24 +1,32 @@
 module github.com/tumasgiu/kokemus
 
 require (
-	github.com/BurntSushi/toml v0.3.1 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	cloud.google.com/go v0.37.2 // indirect
+	github.com/denisenkom/go-mssqldb v0.0.0-20190328043727-2183450503ad // indirect
+	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/gofrs/uuid v3.2.0+incompatible // indirect
 	github.com/google/gopacket v1.1.16
 	github.com/gorilla/handlers v1.4.0
 	github.com/hashicorp/hcl v0.0.0-20171017181929-23c074d0eceb // indirect
+	github.com/jinzhu/gorm v1.9.2
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+	github.com/jinzhu/now v1.0.0 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/lib/pq v1.0.0 // indirect
 	github.com/magiconair/properties v1.7.6 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
 	github.com/mdlayher/raw v0.0.0-20190315120451-8be9e99c38b6 // indirect
 	github.com/mitchellh/mapstructure v0.0.0-20180220230111-00c29f56e238 // indirect
-	github.com/onsi/ginkgo v1.7.0 // indirect
-	github.com/onsi/gomega v1.4.3 // indirect
 	github.com/pelletier/go-toml v1.1.0 // indirect
-	github.com/sirupsen/logrus v1.0.4
+	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/afero v1.0.2 // indirect
 	github.com/spf13/cast v1.2.0 // indirect
 	github.com/spf13/jwalterweatherman v0.0.0-20180109140146-7c0cea34c8ec // indirect
 	github.com/spf13/pflag v1.0.0 // indirect
 	github.com/spf13/viper v1.0.0
 	github.com/stretchr/testify v1.3.0 // indirect
-	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
-	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
+	google.golang.org/appengine v1.5.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 	"github.com/gorilla/handlers"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -18,7 +23,7 @@ import (
 // templ represents a single template
 type templateHandler struct {
 	debug    bool
-	data     interface{}
+	dataMake func() interface{}
 	once     sync.Once
 	filename string
 	templ    *template.Template
@@ -35,7 +40,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				t.filename)))
 		})
 	}
-	t.templ.Execute(w, t.data)
+	t.templ.Execute(w, t.dataMake())
 }
 
 func config() {
@@ -67,17 +72,19 @@ func main() {
 
 func launchRawSniffer() {
 	var (
-		device       string = "eth0"
+		device      string = "eth0"
 		snapshotLen int32  = 1024
-		promiscuous  bool   = false
-		err          error
-		timeout time.Duration = 30 * time.Second
-		handle       *pcap.Handle
+		promiscuous bool   = false
+		err         error
+		timeout     time.Duration = 30 * time.Second
+		handle      *pcap.Handle
 	)
 
 	// Open device
 	handle, err = pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
-	if err != nil {log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer handle.Close()
 
 	// Use the handle as a packet source to process all packets
@@ -88,30 +95,104 @@ func launchRawSniffer() {
 	}
 }
 
+type DbRecord struct {
+	gorm.Model
+	Entry string `json:"entry"`
+}
+
+var db *gorm.DB
+
 func launchHttpServer() {
 	hn, err := os.Hostname()
 	if err != nil {
 		log.Info("could not get hostname")
 	}
 
-	tplData := struct {
-		Hostname string
-		Message  string
-		Color    string
-	}{
-		Hostname: hn,
-		Message:  viper.GetString("message"),
-		Color:    viper.GetString("color"),
+	if viper.GetBool("use_db") {
+		host := viper.GetString("db_host")
+		port := viper.GetString("db_port")
+		user := viper.GetString("db_user")
+		name := viper.GetString("db_name")
+		password := viper.GetString("db_password")
+		sslMode := viper.GetString("db_ssl_mode")
+		gormArgs := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+			host, port, user, name, password, sslMode)
+		log.Info(gormArgs)
+		db, err = gorm.Open("postgres", gormArgs)
+		if err != nil {
+			log.Error(err)
+			os.Exit(2)
+		}
+		db.AutoMigrate(&DbRecord{})
 	}
 
 	http.Handle("/", &templateHandler{
 		filename: "index.html",
 		debug:    viper.GetBool("debug"),
-		data:     tplData,
+		dataMake: func() interface{} {
+			var records []DbRecord
+			db.Find(&records)
+			return struct {
+				Hostname string
+				Message  string
+				Color    string
+				UseDb    bool
+				Records  []DbRecord
+			}{
+				Hostname: hn,
+				Message:  viper.GetString("message"),
+				Color:    viper.GetString("color"),
+				UseDb:    viper.GetBool("use_db"),
+				Records:  records,
+			}
+		},
 	})
+
+	http.HandleFunc("/record", recordHandler)
 
 	err = http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, http.DefaultServeMux))
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
+	}
+	if db != nil {
+		db.Close()
+	}
+}
+
+func recordHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not authorized", http.StatusMethodNotAllowed)
+		return
+	}
+	if r.Body == nil || r.Header.Get("Content-Type") != "application/json" {
+		http.Error(w, "Must have a json body", http.StatusBadRequest)
+		return
+	}
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	record := new(DbRecord)
+	err = json.Unmarshal(data, &record)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := db.Save(&record).Error; err != nil {
+		_, e := w.Write([]byte(fmt.Sprintf(`{ "error": %s }`, strconv.Quote(err.Error()))))
+		if e != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+
+	_, e := w.Write([]byte(fmt.Sprintf(`{ "entry": %s }`, strconv.Quote(record.Entry))))
+	if e != nil {
+		log.Error(e)
 	}
 }
